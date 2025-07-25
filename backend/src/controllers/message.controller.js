5a89db756d64e5480a947fd28759f2da84d697f7
Banx87 <joni.lindholm@gmail.com>
2025-07-25 14:16:06 +0300

    feat: implement messaging functionality with user retrieval, message sending, and message model

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,69 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+	try {
+		// Logic to fetch users for the sidebar
+		const loggedInUserId = req.user._id;
+		const filteredUsers = await User.find({
+			_id: { $ne: loggedInUserId }, // Exclude the logged-in user
+		}).select("-password");
+
+		res.status(200).json(filteredUsers);
+	} catch (error) {
+		console.error("Error in getUsersForSideBar:", error.message);
+		res.status(500).json({ message: "Error fetching users" });
+	}
+};
+
+export const getMessages = async (req, res) => {
+	try {
+		const { id: userToChatId } = req.params;
+		const myId = req.user._id;
+
+		const messages = await Message.find({
+			$or: [
+				{ senderId: myId, receiverId: userToChatId },
+				{ senderId: userToChatId, receiverId: myId },
+			],
+		});
+
+		res.status(200).json(messages);
+	} catch (error) {
+		console.log("Error in getMessages:", error.message);
+		res.status(500).json({ message: "Error fetching messages" });
+	}
+};
+
+export const sendMessage = async (req, res) => {
+	try {
+		const { text, image } = req.body;
+		const { id: receiverId } = req.params;
+		const senderId = req.user._id;
+
+		let imageUrl;
+		if (image) {
+			// Upload base64 image to cloudinary.
+			const uploadResponse = await cloudinary.uploader.upload(image);
+			imageUrl = uploadResponse.secure_url;
+		}
+
+		const newMessage = new Message({
+			senderId,
+			receiverId,
+			text,
+			imagfe: imageUrl,
+		});
+
+		await newMessage.save();
+
+		// TODO: Realtime funciotnality to messaging. => socket.io
+
+		res.status(200).json({
+			newMessage,
+		});
+	} catch (error) {
+		console.log("Error in sendMessage controller: ", error.message);
+		res.status(500).json({ message: "Internal Server Error" });
+	}
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 8001;
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on PORT: ${PORT}`);
