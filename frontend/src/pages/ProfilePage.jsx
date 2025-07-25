fe3d1b2ce23c5e8fc59276ad074cf46fe81745ef
Banx87 <joni.lindholm@gmail.com>
2025-07-25 16:04:13 +0300

    feat: Refactor frontend structure with routing and authentication
    - Added new pages: HomePage, LoginPage, ProfilePage, SettingsPage, SignupPage.
    - Implemented a Navbar component for navigation.
    - Integrated Zustand for state management with an authentication store.
    - Set up Axios instance for API calls.
    - Removed unused App.css and migrated styles to Tailwind CSS.
    - Updated main entry point to include BrowserRouter for routing.
    - Enhanced package.json with new dependencies for Tailwind CSS, Axios, and Zustand.

---
+++ frontend/src/pages/ProfilePage.jsx
@@ -0,0 +1,8 @@
+import { useAuthStore } from "../store/useAuthStore.js";
+
+const ProfilePage = () => {
+	const { authUser } = useAuthStore();
+	return <div>ProfilePage</div>;
+};
+
+export default ProfilePage;
