0204cd88ae48df050f0bbdb732a5b527df46c88f
Banx87 <joni.lindholm@gmail.com>
2025-07-28 15:13:55 +0300

    feat: Implement ChatContainer and MessageInput components with loading states

---
+++ frontend/src/components/ChatContainer.jsx
@@ -0,0 +1,33 @@
+import { useChatStore } from "../store/useChatStore";
+import { useEffect } from "react";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessagesSkeleton from "./skeletons/MessagesSkeleton";
+const ChatContainer = () => {
+	const { messages, getMessages, isMessagesLoading, selectedUser } =
+		useChatStore();
+
+	useEffect(() => {
+		getMessages(selectedUser._id);
+	}, [selectedUser._id, getMessages]);
+
+	if (isMessagesLoading)
+		return (
+			<div className="flex-1 flex flex-col overflow-auto">
+				<ChatHeader />
+				<MessagesSkeleton />
+				<MessageInput />
+			</div>
+		);
+	return (
+		<div className="flex-1 flex flex-col overflow-auto">
+			<ChatHeader />
+
+			<p>Messages...</p>
+
+			<MessageInput />
+		</div>
+	);
+};
+
+export default ChatContainer;
