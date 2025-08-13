85213776322c2bc4797c9f4f9c3555f72b17e6e0
Banx87 <joni.lindholm@gmail.com>
2025-08-13 14:26:46 +0300

    [MOD] Delete unused code

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,14 +1,6 @@
-import React, { use, useState } from "react";
+import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore.js";
-import {
-	Eye,
-	EyeOff,
-	Loader2,
-	Lock,
-	Mail,
-	MessageSquare,
-	User,
-} from "lucide-react";
+import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
